f682f05a8545d05980470d18727758ce4bd0fc8b
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-04-27 23:44:21 -0600

    previous notes now display

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const util = require('util');
-
-const readFromFile = util.promisify(fs.readFile);
+const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -24,35 +22,44 @@ app.get('/notes', (req, res) =>
 );
 
 app.get('/api/notes', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-});
-
-app.post('/api/notes', (req, res) => {
-    console.log("post ", req.body);
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
         } else {
-            const parsedData = JSON.parse(data);
-            parsedData.push(req.body);
-            fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) => {
-                err ? console.error(err) : console.info(`\nData written to ./db/db.json`)
-            });
+            res.send(data);
         }
     });
+});
+
+app.post('/api/notes', (req, res) => {
+    console.log("post ", req.body);
+
+    const { title, text } = req.body;
+
+    // adding id to post
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedData = JSON.parse(data);
+                parsedData.push(newNote);
+                fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) => {
+                    err ? console.error(err) : console.info(`\nData written to ./db/db.json`)
+                });
+            }
+        });
+    }
+
+    res.send('');
 });
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-
-/* 
-WHEN I enter a new note title and the note’s text
-THEN a Save icon appears in the navigation at the top of the page
-WHEN I click on the Save icon
-THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-WHEN I click on an existing note in the list in the left-hand column
-THEN that note appears in the right-hand column
-WHEN I click on the Write icon in the navigation at the top of the page
-THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
-*/
